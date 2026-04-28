06ed675fa4bb1cb43dd27fc49364e405dfbaa5db
thichcodedao2006 <truongpd.a2.2124@gmail.com>
2026-04-28 11:20:49 +0700

    Move Char and camera follow

--- Assets/Script/Player/PlayerMovement.cs
+++ Assets/Script/Player/PlayerMovement.cs
@@ -4,15 +4,62 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    #region Declare 
+    [Header ("Property")]
+    public float PlayerSpeed;
+    [Header("Self Defined")]
+    private Rigidbody2D rb;
+    private Animator animator;
+    private Vector2 Direction;
+    private float lastX, lastY;
+    #endregion
+
+    #region Update
+    private void Awake()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
-        
+        lastX = 0;
+        lastY = -1;
+        animator.SetFloat("lastX", lastX);
+        animator.SetFloat("lastY", lastY);
     }
+    private void Update()
+    {
+        GetDirection();
+    }
+    private void FixedUpdate()
+    {
+        Move();
+    }
+    #endregion
+
+    #region Function
+    private void Move()
+    {
+        rb.velocity = PlayerSpeed * Direction;  
+    }
+
+    private void GetDirection()
+    {
+        float x = Input.GetAxisRaw("Horizontal");
+        float y = Input.GetAxisRaw("Vertical");
+        if (x != 0 || y != 0)
+        {
+            animator.SetBool("IsMoving", true);
+            lastX = x;
+            lastY = y;
+            animator.SetFloat("lastX", lastX);
+            animator.SetFloat("lastY", lastY);
+        }
+        else animator.SetBool("IsMoving", false);
+       Direction = new Vector2(x, y).normalized;
+        animator.SetFloat("moveX", x);
+        animator.SetFloat("moveY", y);
+    }
+    #endregion
 }
